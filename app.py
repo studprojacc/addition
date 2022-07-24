5c7b1373126418b9a5f2d6584dfac739ae77e74b
studprojacc <109853730+studprojacc@users.noreply.github.com>
2022-07-24 19:21:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.header('User Input Parameters')
 
 with st.form(key='my_form'):
     first = st.number_input("First_Number",step=1)
-    second = st.number_input("Second_Number")
+    second = st.number_input("Second_Number",step=1)
     submit= st.form_submit_button(label='Submit')
 if submit:
     st.write(first-second)
